5f6ecc927fbc1b14ca0b92e56ad0c620afe58413
Trianglebucks <blazerx1234@gmail.com>
2023-06-19 12:30:40 +0800

    add expense tracker project

--- src/helpers/ProjectList.jsx
+++ src/helpers/ProjectList.jsx
@@ -6,6 +6,8 @@ import FloWord_Gameplay from "../assets/FloWord_Gameplay.jpg";
 import Atlas_Login from "../assets/Atlas_Login.jpg";
 import Jobify_Login from "../assets/Jobify_Login.jpg";
 import Kampo_Home from "../assets/Kampo_Home.jpg";
+import Expense_Home from "../assets/Expense_Home.png";
+import Expense_Thumb from "../assets/Expense_Thumb.png";
 
 export const ProjectList = [
   {
@@ -13,26 +15,33 @@ export const ProjectList = [
     thumb: FloWord_Thumb,
     display: FloWord_Gameplay,
     skills: "C#, Unity Engine, Firebase",
-    link: "https://github.com/Trianglebucks/hangman_v1-feedback"
+    link: "https://github.com/Trianglebucks/hangman_v1-feedback",
   },
   {
     name: "Atlas Inventory ",
     thumb: Atlas_Thumb,
     display: Atlas_Login,
     skills: "C#, SQLite, WPF",
-    link: "https://github.com/Trianglebucks/AtlasProject"
+    link: "https://github.com/Trianglebucks/AtlasProject",
   },
   {
     name: "Jobify",
     thumb: Jobify_Thumb,
     display: Jobify_Login,
     skills: "MERN Stack (MongoDB, Express, React, Node)",
-    link: "https://mern-jobify-dlub.onrender.com/landing"
+    link: "https://mern-jobify-dlub.onrender.com/landing",
   },
   {
     name: "Kampo Apparel ",
     thumb: Kampo_Thumb,
     display: Kampo_Home,
-    skills: "HTML, CSS, Bootstrap, PHP, MySQL"
-  }
+    skills: "HTML, CSS, Bootstrap, PHP, MySQL",
+  },
+  {
+    name: "Expense Tracker",
+    thumb: Expense_Thumb,
+    display: Expense_Home,
+    skills: "MERN Stack (MongoDB, Express, React, Node)",
+    link: "https://expense-tracker-kpue.onrender.com/",
+  },
 ];
